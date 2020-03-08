fd860a20f5ce49d7835b75a5954a42203311d414
bruegger <info@lbo.ch>
2020-03-08 20:13:02 +0100

    - remove backend function

--- database/database.php
+++ database/database.php
@@ -4,14 +4,12 @@
  * Class database
  * used for manage the database
  */
-class database
-{
+class database {
     /**
      * @return mysqli
      */
-    public static function getConnection()
-    {
-        $connection = mysqli_connect("localhost", "root", "root", "bc");
+    public static function getConnection() {
+        $connection = mysqli_connect("localhost", "ch263738_boersencrash", "Assd#[]dfcmdlkf343434", "ch263738_boersencrash");
         return $connection;
     }
 
@@ -20,10 +18,18 @@ class database
      * @param $connection
      * @return mixed
      */
-    public static function getResultByQuery($query, $connection)
-    {
+    public static function getResultByQuery($query, $connection) {
         $result = $connection->query($query);
         return $result;
+    }
+
+    public static function getDrinks($connection) {
+        $result = $connection->query("SELECT * FROM drink");
+        return $result;
+    }
+
+    public static function updateDrinkPrice($id, $price, $connection) {
+        $result = $connection->query("UPDATE `drink` SET `currentPrice` = '$price' WHERE `drink`.`id` = $id; ");
     }
 
 }
--- frontend/index.php
+++ frontend/index.php
@@ -1,89 +1,71 @@
 <head>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-		  integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<link rel="stylesheet" href="style/style.css">
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-			integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-			crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-			integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-			crossorigin="anonymous"></script>
-	<meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/style.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+            integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+            crossorigin="anonymous"></script>
+    <meta charset="UTF-8">
 </head>
 <body>
 <?php
 include '../database/database.php';
 include 'alert.php';
 $connection = database::getConnection();
-$drinkList = database::getResultByQuery("SELECT id, name, minPrice, maxPrice, currentPrice, soldUnits, salesTime FROM drink", $connection);
+$drinkList = database::getDrinks($connection);
 
 if ($drinkList->num_rows > 0) {
-    // output data of each row
     while ($drink = $drinkList->fetch_assoc()) {
-
-        $fk_drinkId = $drink["id"];
-        $id = $drink["id"];
-        $averageList = database::getResultByQuery("SELECT AVG(salesTime) AS average FROM ( SELECT * FROM salesHistory WHERE fk_drinkId='$fk_drinkId' ORDER BY id DESC LIMIT 7 ) AS average", $connection);
-        while ($item = $averageList->fetch_assoc()) {
-            $average = $item["average"];
-        }
-        $percent = (100 / $average) * $drink["salesTime"];
-        //echo "<br> percent: " . $percent . "<br>";
-        $drinkCurrentPrice = $drink["currentPrice"];
-        if ($percent < 50) {
-            $currentPrice = $drinkCurrentPrice + 1;
-        } elseif ($percent < 75) {
-            $currentPrice = $drinkCurrentPrice + 0.5;
-        } elseif ($percent < 125) {
-            $currentPrice = $drinkCurrentPrice - 0.5;
-        } elseif ($percent < 200) {
-            $currentPrice = $drinkCurrentPrice - 1;
-        } else {
-            $currentPrice = $drinkCurrentPrice;
-        }
-        //add price to pricehistory table
-        database::getResultByQuery(" INSERT INTO priceHistory( fk_drinkId, price) VALUES ('$fk_drinkId', '$currentPrice')", $connection);
-        $salesTimeList = database::getResultByQuery(" SELECT * FROM drink WHERE id='$fk_drinkId' ORDER BY id DESC LIMIT 1", $connection);
-        while ($item = $salesTimeList->fetch_assoc()) {
-            $salesTime = $item["salesTime"] + 1;
-        }
-        // add salestime
-        database::getResultByQuery("UPDATE drink SET salesTime='$salesTime' WHERE id='$id'", $connection);
-        // add salestimehistory
-        database::getResultByQuery("INSERT INTO salesHistory( fk_drinkId, salesTime, time) VALUES ('$fk_drinkId', '$salesTime', '0')", $connection);
-        $minPrice = $drink["minPrice"];
-        $maxPrice = $drink["maxPrice"];
-
-        if ($currentPrice <= $minPrice) {
-            $name = $drink["name"];
-            if ($currentPrice >= $minPrice) {
-                $currentPrice = $minPrice;
-
-            }
-            alert::boersencrash($name, $currentPrice);
-        } else {
-            if ($currentPrice > $maxPrice) {
-                $currentPrice = $maxPrice;
-            }
-            database::getResultByQuery("UPDATE drink SET currentPrice='$currentPrice' WHERE id='$id'", $connection);
-            $name = $drink["name"];
-            $random = rand(0, 100);
-            if ($random !== 1) {
-                if ($currentPrice <= $minPrice + 2) {
-                    alert::warning($name, $currentPrice);
-                } elseif ($currentPrice >= $maxPrice - 3) {
-                    alert::success($name, $currentPrice);
+        $part = ($drink["maxPrice"] - $drink["minPrice"]) / 3;
+        if ($drink["currentPrice"] < $drink["minPrice"] + $part) {
+            if (rand(1, 3) == 3) {
+                if ($drink["minPrice"] > $drink["currentPrice"] - 0.5) {
+                    $price = $drink["currentPrice"];
                 } else {
-                    alert::normal($name, $currentPrice);
+                    $price = $drink["currentPrice"] - 0.5;
                 }
             } else {
-                alert::boersencrash($name, $currentPrice);
+                $price = $drink["currentPrice"] + 0.5;
             }
-
         }
+        if ($drink["currentPrice"] > $drink["minPrice"] + $part && $drink["currentPrice"] < $drink["maxPrice"] - $part) {
+            if (rand(1, 2) == 2) {
+                if ($drink["minPrice"] > $drink["currentPrice"] - 0.5) {
+                    $price = $drink["currentPrice"];
+                } else {
+                    $price = $drink["currentPrice"] - 0.5;
+                }
+            } else {
+                $price = $drink["currentPrice"] + 0.5;
+            }
+        }
+        if ($drink["currentPrice"] > $drink["maxPrice"] - $part) {
+            if (rand(1, 3) == 3) {
+                if ($drink["maxPrice"] < $drink["currentPrice"] + 0.5) {
+                    $price = $drink["currentPrice"];
+                } else {
+                    $price = $drink["currentPrice"] + 0.5;
+                }
+            } else {
+                $price = $drink["currentPrice"] - 0.5;
+            }
+        }
+        database::updateDrinkPrice($drink["id"], $price, $connection);
+
+        echo $drink["name"];
+        echo "<br>";
+        echo $drink["currentPrice"];
+        echo "<hr>";
+
+        //alert::normal($name, $currentPrice);
+        $drinkCurrentPrice = $drink["currentPrice"];
+        //alert::normal($name, $drinkCurrentPrice);
     }
 } else {
-    alert::error("No drinks registert");
+    alert::error("No drinks registered");
 }
 header("refresh: 60;");
 ?>
